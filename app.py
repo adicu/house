825e3dd9fc5f77f8513216f3fbc5bd40f5c0933d
Nate Brennand <natebrennand@gmail.com>
2013-09-02 01:59:25 -0400

    flesh out send_text method

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from os import environ
+import datetime as DT
 import simplejson as JSON
 import requests
 
@@ -9,9 +10,19 @@ app.debug = True
 GROUPME_TOKEN = environ.get('GROUPME_TOKEN')
 GROUPME_BOT_ID = environ.get('GROUPME_BOT_ID')
 
+TIMEOUT_MINUTES = 30
+
 
 @app.route('/be_quiet', methods=['POST'])
 def send_groupme():
+    last_text = DT.datetime.strptime(request.cookies.get('last_text'))
+    now = DT.datetime.today()
+
+    diff =  now - last_text
+    if diff.seconds/60 < TIMEOUT_MINUTES:
+        return 'Sorry, you must wait {} minutes before sending another text', 403
+
+
     url = 'https://api.groupme.com/v3/bots/post'
     payload = {
         'bot_id' : GROUPME_BOT_ID,
@@ -21,10 +32,17 @@ def send_groupme():
     headers = {
         'Content-Type' : 'Application/json'
     }
+    #resp = requests.post(url, data=(JSON.dumps(payload)), headers=headers)
+    print url, payload, headers
 
-    resp = requests.post(url, data=(JSON.dumps(payload)), headers=headers)
-    print resp
 
+    #if resp.status_code == 201:
+    if True:
+        response = make_response()
+        response.set_cookie('last_text', DT.datetime.today().utcnow())
+        return response
+    else:
+        return 'Error posting message to GroupMe', 501
 
 @app.route('/')
 def main():
--- src/js/controllers/HomeCtrl.coffee
+++ src/js/controllers/HomeCtrl.coffee
@@ -1,3 +1,14 @@
 angular.module('app.controllers')
 .controller 'HomeCtrl', ($scope) ->
-  $scope.actionCalled = false
+	$scope.actionCalled = false
+
+	$scope.message = "Please be quiet"
+
+	$scope.sendText = () -> 
+		data = message : $scope.message
+		$http.post('/be_quiet', data)
+			.sucess (data) ->
+				console.log data
+			.error (err) ->
+				console.log err
+
