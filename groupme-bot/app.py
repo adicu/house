3b0c8489acd67d61d5066d54971a56f7acdecf3e
Rasmi Elasmar <rrelasmar@gmail.com>
2015-04-03 19:31:58 -0400

    Attempting handshake to try polling instead of waiting for GroupMe POSTs because Columbia are fascists.

--- groupme-bot/app.py
+++ groupme-bot/app.py
@@ -3,17 +3,29 @@ import requests
 
 app = Flask(__name__)
 
+def handshake():
+    data = {
+        'channel':'/meta/handshake',
+        'version':'1.0',
+        'supportedConnectionTypes':['long-polling'],
+        'id':'1'
+    }
+    r = requests.post('https://push.groupme.com/faye', data=data)
+
+    if r.status_code == requests.codes.ok:
+        return r.content
+
 @app.route('/', methods=['POST'])
 def handle_message():
-	text = request.form['text']
-	sender = request.form['sender']
+    text = request.form['text']
+    sender = request.form['sender']
 
-	display_message(sender, text)
+    display_message(sender, text)
 
-	pass
+    pass
 
 def display_message(sender, text):
-	pass
+    pass
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
