552e4d54360f1922f0550765ba5520f82eba8b53
Dan Schlosser <dan.r.schlosser@gmail.com>
2015-04-03 22:54:12 -0400

    add text message reading to app.py by default

--- groupme-bot/app.py
+++ groupme-bot/app.py
@@ -1,19 +1,32 @@
 from flask import Flask, Response, request
 from displays import functions
+from images import display_text
 import json
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['POST'])
 def handle_message():
     data = json.loads(request.data)
 
-    text =  data['text']
+    text = data['text']
     name = data['name']
 
-    functions[name](text)
+    if name in functions:
+        functions[name](text)
 
-    return Response(status=200)    
+    display_text_message(name, text)
+
+    return Response(status=200)
+
+
+def display_text_message(name, text):
+    display_text([
+        (name + ": ", (255, 0, 0)),
+        (text, (0, 255, 0))
+    ])
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
